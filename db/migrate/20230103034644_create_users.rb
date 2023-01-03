f88bd354471c94348a1ba2c881a833776063696e
KurangKering <cybercature@gmail.com>
2023-01-03 11:15:25 +0700

    make user model and the migration file

---
+++ db/migrate/20230103034644_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users, id: false do |t|
+      t.integer :id_user, null: false, primary_key: true
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :phone_number
+      t.string :password, null: false
+      t.string :forgot_password_token
+      t.integer :fake_otp
+      t.date :fake_otp_expired_time
+      t.timestamps
+    end
+
+  end
+end
